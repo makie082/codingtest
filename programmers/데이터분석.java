d975efacfa64a63e1b0093eab43f29d51ede5871
mimimoo <75782242+makie082@users.noreply.github.com>
2024-03-12 00:30:58 +0900

    level1_sort

--- programmers/데이터분석.java
+++ programmers/데이터분석.java
@@ -62,3 +62,33 @@ class Solution {
         }
     }
 }
+
+
+// 더 효율적인 풀이~!!
+import java.util.*;
+
+class Solution {
+    public List<int[]> solution(int[][] data, String ext, int val_ext, String sort_by) {
+
+        List<int[]> list = new ArrayList<>();
+        String[] s = {"code","date","maximum","remain"};
+        int extIdx=0, sortIdx=0;
+
+        for(int i=0; i<4; i++){
+            if(ext.equals(s[i])) extIdx=i;
+            if(sort_by.equals(s[i])) sortIdx=i;
+        }
+        final int si=sortIdx;
+
+        for(int i=0; i<data.length; i++){
+            if(data[i][extIdx]<val_ext) list.add(data[i]); // list에 데이터 값들 저장
+        }
+
+        // *lambda, Comparator 사용시 final 이나 effectively final여야한다!
+        Collections.sort(list,(o1,o2)->o1[si]-o2[si]); // 해당 기준으로 sort
+
+
+        return list;
+    }
+}
+
